2778985254b34da771699a67b052a95e8b1f5be5
shinjia <cxnap713@gmail.com>
2025-02-10 03:13:18 +0900

    commit

--- fakeNews/fakeNews/celery.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-import os
- 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fakeNews.settings')
-
-app = Celery('fakeNews')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.autodiscover_tasks()  # ← 태스크 자동 발견 추가!
--- fakeNews/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main, name='main'), # 메인
-    path('generate_news/', views.generate_news_view, name='generate_news'), # 뉴스 생성
-    path('task_status/<str:task_id>/', views.check_task_status, name='check_task_status'), # 생성된 뉴스 확인
-]
